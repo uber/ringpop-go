6381607b48280cbe6bbe4a5b3f2242036e4b7fc4
Simon Lantinga <slantin@uber.com>
2015-06-29 18:00:04 -0700

    make gossip do something; fix data race

--- gossip.go
+++ gossip.go
@@ -3,6 +3,7 @@ package ringpop
 import (
 	"math"
 	"math/rand"
+	"sync"
 	"time"
 
 	log "github.com/Sirupsen/logrus"
@@ -10,7 +11,7 @@ import (
 	"github.com/rcrowley/go-metrics"
 )
 
-const defaultMinProtocolPeriod = time.Millisecond * 200
+const defaultMinProtocolPeriod = time.Millisecond * 1500
 
 //= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 //
@@ -30,6 +31,8 @@ type gossip struct {
 	protocolRateTimer  *time.Ticker
 
 	protocolTiming metrics.Histogram
+
+	lock sync.Mutex // protects stopped flag
 }
 
 func newGossip(ringpop *Ringpop, minProtocolPeriod time.Duration) *gossip {
@@ -41,7 +44,7 @@ func newGossip(ringpop *Ringpop, minProtocolPeriod time.Duration) *gossip {
 		ringpop:           ringpop,
 		stopped:           true,
 		minProtocolPeriod: minProtocolPeriod,
-		protocolTiming:    metrics.NewHistogram(metrics.NilSample{}),
+		protocolTiming:    metrics.NewHistogram(metrics.NewUniformSample(10)), // best size for this?
 	}
 
 	gossip.protocolTiming.Update(int64(gossip.minProtocolPeriod))
@@ -55,79 +58,96 @@ func newGossip(ringpop *Ringpop, minProtocolPeriod time.Duration) *gossip {
 //
 //= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
+func (g *gossip) Stopped() bool {
+	g.lock.Lock()
+	defer g.lock.Unlock()
+	return g.stopped
+}
+
+func (g *gossip) SetStopped(stopped bool) {
+	g.lock.Lock()
+	defer g.lock.Unlock()
+	g.stopped = stopped
+}
+
 func (g *gossip) computeProtocolDelay() time.Duration {
 	if g.numProtocolPeriods != 0 {
 		target := g.lastProtocolPeriod.Add(g.lastProtocolRate)
-		return time.Duration(math.Max(float64(target.UnixNano()-time.Now().UnixNano()), float64(g.minProtocolPeriod))) // REVISIT
+		return time.Duration(math.Max(float64(target.UnixNano()-time.Now().UnixNano()), float64(g.minProtocolPeriod)))
 	}
-
 	// delay for first tick staggered in [0, minProtocolPeriod] ms
 	return time.Duration(math.Floor(rand.Float64() * float64(g.minProtocolPeriod+1)))
 }
 
 func (g *gossip) computeProtocolRate() time.Duration {
-	observed := g.protocolTiming.Percentiles([]float64{0.5})[1] * 2.0
+	observed := g.protocolTiming.Percentile(0.5) * 2.0
 	return time.Duration(math.Max(observed, float64(g.minProtocolPeriod)))
 }
 
 // TODO
 func (g *gossip) run() {
+	startTime := time.Now()
+
 	go func() {
 		for {
+			if g.Stopped() {
+				g.ringpop.logger.WithField("local", g.ringpop.WhoAmI()).
+					Debug("[ringpop] stopped recurring gossip loop")
+				break
+			}
+
 			protocolDelay := g.computeProtocolDelay()
 			g.ringpop.stat("timing", "protocol.delay", milliseconds(protocolDelay))
 			time.Sleep(protocolDelay)
 
-			if g.stopped {
-				g.ringpop.logger.WithField("local", g.ringpop.WhoAmI()).
-					Debug("stopped recurring gossip loop")
-				break
-			}
+			pingStartTime := time.Now()
 
-			// pingStartTime := time.Now()
-			// err := g.ringpop.PingMemberNow()
+			g.ringpop.PingMemberNow()
 
 			g.lastProtocolPeriod = time.Now()
 			g.numProtocolPeriods++
+			// TODO: stat call
+			g.ringpop.stat("timing", "protocol.frequency", unixMilliseconds(startTime))
+			g.protocolTiming.Update(int64(time.Now().Sub(pingStartTime))) // keeps protocol rate in check
 		}
 	}()
 }
 
 func (g *gossip) start() {
-	if !g.stopped {
+	if !g.Stopped() {
 		g.ringpop.logger.WithFields(log.Fields{
 			"local": g.ringpop.WhoAmI(),
-		}).Debug("gossip has already started")
+		}).Debug("[ringpop] gossip has already started")
 
 		return
 	}
 
-	// g.ringpop.membership.shuffle() // REVISIT
+	g.ringpop.membership.shuffle()
 	g.run()
 	g.startProtocolRateTimer()
-	g.stopped = false
+	g.SetStopped(false)
 
 	g.ringpop.logger.WithFields(log.Fields{
 		"local": g.ringpop.WhoAmI(),
-	}).Debug("started gossip protocol")
+	}).Debug("[ringpop] started gossip protocol")
 }
 
 func (g *gossip) stop() {
-	if g.stopped {
+	if g.Stopped() {
 		g.ringpop.logger.WithFields(log.Fields{
 			"local": g.ringpop.WhoAmI(),
-		}).Debug("gossip is already stopped")
+		}).Debug("[ringpop] gossip is already stopped")
 
 		return
 	}
 
 	g.protocolRateTimer.Stop()
 
-	g.stopped = true
+	g.SetStopped(true)
 
 	g.ringpop.logger.WithFields(log.Fields{
 		"local": g.ringpop.WhoAmI(),
-	}).Debug("stopped gossip protocol")
+	}).Debug("[ringpop] stopped gossip protocol")
 }
 
 // StartProtocolRateTimer creates a ticker and launches a goroutine that
