e85c554f7ec3d03f24b5aab6db09bcd05fabf25b
Nils Dijk <nils@uber.com>
2015-12-30 17:28:41 +0100

    use tchannel-go release of thrift-gen

--- scripts/travis/get-thrift-gen.sh
+++ scripts/travis/get-thrift-gen.sh
@@ -4,5 +4,5 @@ set -e
 
 cd "$(dirname "$0")"
 rm -rf thrift-gen-release.tar.gz
-wget https://github.com/thanodnl/tchannel-go/releases/download/v0.2-alpha/thrift-gen-release.tar.gz
+wget https://github.com/uber/tchannel-go/releases/download/v0.01/thrift-gen-release.tar.gz
 tar -xzf thrift-gen-release.tar.gz
