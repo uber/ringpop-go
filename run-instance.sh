ec266333154d5ca1f42770f94d04d0cb07694454
Menno Pruijssers <menno@uber.com>
2016-01-29 17:37:12 +0100

    basic health check + interupt handler

--- run-instance.sh
+++ run-instance.sh
@@ -23,13 +23,54 @@ ringpop_bind="$BIND:$ringpop_port"
 
 echo "Starting serf on $serf_bind (rpc: $serf_rpc_bind)..."
 $SERF agent -bind=$serf_bind -rpc-addr=$serf_rpc_bind -node=$serf_node -tag rphost=$BIND -tag rpport=$ringpop_port -event-handler=./serf_events/eh.py &
+serf_pid=$!
 
-sleep 5
 echo "Starting ringpop ($ringpop_bind)"
 $RINGPOP -listen $ringpop_bind &
+ringpop_pid=$!
 
 if [ $instance_no -ne 0 ]; then
 	echo "Joining $serf_master"
 	$SERF join -rpc-addr=$serf_rpc_bind $serf_master
 fi
 
+echo "serf_pid: $serf_pid, ringpop_pid: $ringpop_pid"
+running=1
+
+health_check() {
+	if ! kill -0 $serf_pid > /dev/null 2>&1; then
+		echo "serf down"
+		return -1
+	fi
+
+	if ! kill -0 $ringpop_pid > /dev/null 2>&1; then
+		echo "ringpop down"
+		return -2
+	fi
+
+	return 0
+}
+
+trap_handler() {
+	running=0
+}
+
+quit() {
+	echo "Quiting..."
+	kill $serf_pid > /dev/null 2>&1
+	kill $ringpop_pid > /dev/null 2>&1
+}
+
+trap trap_handler INT
+
+while health_check
+do
+	if [ $running -ne 1 ]; then
+		break
+	fi
+
+	echo "Health check passed"
+	sleep 1
+done
+
+quit
