69e7da1065ee7546187cfe5bd83fc7fa0e082f87
Daniel Simmons <dans@uber.com>
2016-02-24 22:21:21 +0100

    Cache directories on Travis

--- scripts/travis/get-thrift.sh
+++ scripts/travis/get-thrift.sh
@@ -5,5 +5,4 @@ set -e
 cd "$(dirname "$0")"
 rm -rf thrift-release.zip
 wget https://github.com/prashantv/thrift/releases/download/p0.0.1/thrift-release.zip
-unzip thrift-release.zip
-
+unzip -o thrift-release.zip
