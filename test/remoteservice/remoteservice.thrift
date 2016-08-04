19ff2e65a88aa24bab0e490ee355972d70d8f7bb
Nils Dijk <me@thanod.nl>
2016-08-04 14:37:00 +0200

    Support imported files in thrift-gen (#162)
    * add extra imports included in thrift file in a way that prevents unused errors during compiling
    
    * Add import to test for generated code.
    
    * Add an actual unused import to make sure that thrift-gen generates valide code.
    
    * Add extra explanation to the unused.thift import, why it is significant
    
    * Use shared.Name instead of shared.UUID to reduce changes and keep consistent

--- test/remoteservice/remoteservice.thrift
+++ test/remoteservice/remoteservice.thrift
@@ -1,3 +1,11 @@
+include "./shared.thrift"
+
+// the unused file contains types that are not used in service endpoints. These
+// types might actually be used in embedded structs here, but since the
+// generated code could potentially contain the import of an unused package in
+// the RingpopAdapter. To prevent this the generator uses `GoUnusedProtection__`
+include "./unused.thrift"
+
 service RemoteService {
-  void RemoteCall(1: string name)
+  void RemoteCall(1: shared.Name name)
 }
