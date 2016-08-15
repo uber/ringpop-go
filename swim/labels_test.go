55be11d0a9b3a4f2893f9ef54f7aa3e46d819006
Nils Dijk <nils@uber.com>
2016-08-15 18:45:57 +0200

    add tests for validating multiple labels at once.

--- swim/labels_test.go
+++ swim/labels_test.go
@@ -25,3 +25,26 @@ func TestValidateLabel(t *testing.T) {
 		assert.Equal(t, test.err, validateLabel(test.key, test.value), "key: %q value: %q expected: %+v", test.key, test.value, test.err)
 	}
 }
+
+var validateLabelsTests = []struct {
+	labels map[string]string
+	err    error
+}{
+	{map[string]string{"hello": "world"}, nil},
+	{map[string]string{"hello": "world="}, nil},
+	{map[string]string{"hello": "wor=ld"}, nil},
+	{map[string]string{"hello": "=world"}, nil},
+	{map[string]string{"hello=": "world"}, ErrLabelInvalidKey},
+	{map[string]string{"hel=lo": "world"}, ErrLabelInvalidKey},
+	{map[string]string{"=hello": "world"}, ErrLabelInvalidKey},
+
+	{map[string]string{"foo": "bar", "hello=": "world"}, ErrLabelInvalidKey},
+	{map[string]string{"foo": "bar", "hel=lo": "world"}, ErrLabelInvalidKey},
+	{map[string]string{"foo": "bar", "=hello": "world"}, ErrLabelInvalidKey},
+}
+
+func TestValidateLabels(t *testing.T) {
+	for _, test := range validateLabelsTests {
+		assert.Equal(t, test.err, validateLabels(test.labels), "labels: %v expected: %+v", test.labels, test.err)
+	}
+}
