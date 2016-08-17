5a725369579ea16ae8cfdeba2012b27025bb4d21
Nils Dijk <nils@uber.com>
2016-08-17 14:53:44 +0200

    go imports

--- swim/labels_test.go
+++ swim/labels_test.go
@@ -3,9 +3,8 @@ package swim
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
-
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNodeLabels(t *testing.T) {
