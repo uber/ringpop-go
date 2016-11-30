7d33825ccd81facbf9134396dc0ba23e61386968
Nils Dijk <nils@uber.com>
2016-11-30 17:13:28 +0100

    Revisit the documentation of the Member interface.

--- membership/interface.go
+++ membership/interface.go
@@ -1,11 +1,16 @@
 package membership
 
-// Member defines a member of the membership that can be used by the rest of the
-// system
+// Member defines a member of the membership. It can be used by applications to
+// apply specific business logic on Members. Examples are:
+// - Get the address of a member for RPC calls, both forwarding of internal
+//   calls that should target a Member
+// - Decissions to include a Member in a query via predicates.
 type Member interface {
 	// GetAddress returns the external address used by the rpc layer to
-	// communicate to the member. It is prefixed with Get for legacy reasons and
-	// can be removed after a refactor of the swim.Member
+	// communicate to the member.
+	//
+	// Note: It is prefixed with Get for legacy reasons and can be removed after
+	// a refactor of the swim.Member to free up the `Address` name.
 	GetAddress() string
 
 	// Label reads the label for a given key from the member. It also returns
