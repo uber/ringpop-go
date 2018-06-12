3444881c4ba517e333b27a3deecad4383acec5f8
Joshua T Corbin <joshua@uber.com>
2018-06-12 10:20:47 -0700

    Track max-seen forwarding buffer size, and allocate that much going forward (modulo sanity)

--- forward/request_sender.go
+++ forward/request_sender.go
@@ -27,6 +27,7 @@ import (
 	"io"
 	"io/ioutil"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"golang.org/x/net/context"
@@ -161,21 +162,30 @@ func (s *requestSender) Send() (res []byte, err error) {
 
 type _bufPool struct {
 	sync.Pool
+	maxCap int64
 }
 
 func (bp *_bufPool) Get() *bytes.Buffer {
+	const allocLimit = 100 * 1024 * 1024 // cap initial allocations at 100MiB
 	val := bp.Pool.Get()
 	if buf, ok := val.(*bytes.Buffer); ok {
 		return buf
 	}
-	return bytes.NewBuffer(make([]byte, 0, 64*1024))
+	n := atomic.LoadInt64(&bp.maxCap)
+	if n > allocLimit {
+		n = allocLimit
+	}
+	return bytes.NewBuffer(make([]byte, 0, n))
 }
 
 func (bp *_bufPool) Put(buf *bytes.Buffer) {
+	if n, m := int64(buf.Cap()), atomic.LoadInt64(&bp.maxCap); n > m {
+		atomic.CompareAndSwapInt64(&bp.maxCap, m, n)
+	}
 	bp.Pool.Put(buf)
 }
 
-var bufPool = _bufPool{}
+var bufPool = _bufPool{maxCap: 64 * 1024}
 
 // calls remote service and writes response to s.response
 func (s *requestSender) MakeCall(ctx context.Context, res *[]byte, fwdError *error, appError *error) <-chan bool {
