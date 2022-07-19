78b2151a3f6ec07afc79a755968945a936f5185d
Krishna Gupta <gkrishna@uber.com>
2022-07-19 23:02:03 +0530

    Updated tchannel-go to 1.32.0, regen thrift files

--- examples/keyvalue/gen-go/keyvalue/tchan-keyvalue.go
+++ examples/keyvalue/gen-go/keyvalue/tchan-keyvalue.go
@@ -6,7 +6,7 @@ package keyvalue
 import (
 	"fmt"
 
-	athrift "github.com/apache/thrift/lib/go/thrift"
+	athrift "github.com/uber/tchannel-go/thirdparty/github.com/apache/thrift/lib/go/thrift"
 	"github.com/uber/tchannel-go/thrift"
 )
 
